9126907d056e07ade8a7bcb7b4d24a2acd39b125
Ardhi Harry <ardhiharry@gmail.com>
2023-12-22 23:36:49 +0700

    add category to route

--- src/route/index.js
+++ src/route/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import cartProductRoute from '../cartProduct/cartProduct.route.js';
+import categoryRoute from '../category/category.route.js';
 
 const router = express.Router();
 
 // CART PRODUCT
 router.use(cartProductRoute);
+
+// CATEGORY PRODUCT
+router.use(categoryRoute);
 
 router.get('/hello', (req, res) => {
   res.status(200).json({
