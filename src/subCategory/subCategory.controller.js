27a8268f9a70f8586c0f71cef2b2e1bcdd992ee0
Ardhi Harry <ardhiharry@gmail.com>
2023-12-23 12:19:08 +0700

    memperbarui response error dan response success

--- src/subCategory/subCategory.controller.js
+++ src/subCategory/subCategory.controller.js
@@ -1,19 +1,22 @@
 import subCategoryService from './subCategory.service.js';
-import ResponseError from '../../helper/responseError.js';
+import ResponseJson from '../../helper/responseJson.js';
 
 async function createSubCategory(req, res) {
   try {
     const subCategory = await subCategoryService.create(req);
 
-    return res.status(201).json({
-      status: 'success',
-      message: 'success create sub category',
-      data: {
-        subCategory,
-      },
-    });
+    return ResponseJson.responseSuccess(
+      res,
+      201,
+      'success create sub category',
+      { subCategory },
+    );
   } catch (err) {
-    return ResponseError(res, err.statusCode || 400, err.message);
+    return ResponseJson.responseError(
+      res,
+      err.statusCode || 400,
+      err.message,
+    );
   }
 }
 
@@ -21,15 +24,18 @@ async function getAllSubCategory(req, res) {
   try {
     const subCategory = await subCategoryService.getAll(req);
 
-    return res.status(200).json({
-      status: 'success',
-      message: 'success get all sub category',
-      data: {
-        subCategory,
-      },
-    });
+    return ResponseJson.responseSuccess(
+      res,
+      200,
+      'success get all sub category',
+      { subCategory },
+    );
   } catch (err) {
-    return ResponseError(res, err.statusCode || 400, err.message);
+    return ResponseJson.responseError(
+      res,
+      err.statusCode || 400,
+      err.message,
+    );
   }
 }
 
@@ -37,15 +43,18 @@ async function getSubCategoryById(req, res) {
   try {
     const subCategory = await subCategoryService.getById(req);
 
-    return res.status(200).json({
-      status: 'success',
-      message: 'success get sub category detail',
-      data: {
-        subCategory,
-      },
-    });
+    return ResponseJson.responseSuccess(
+      res,
+      200,
+      'success get sub category detail',
+      { subCategory },
+    );
   } catch (err) {
-    return ResponseError(res, err.statusCode || 400, err.message);
+    return ResponseJson.responseError(
+      res,
+      err.statusCode || 400,
+      err.message,
+    );
   }
 }
 
@@ -53,15 +62,18 @@ async function updateSubCategory(req, res) {
   try {
     const subCategory = await subCategoryService.update(req);
 
-    return res.status(200).json({
-      status: 'success',
-      message: 'success update sub category',
-      data: {
-        subCategory,
-      },
-    });
+    return ResponseJson.responseSuccess(
+      res,
+      200,
+      'success update sub category',
+      { subCategory },
+    );
   } catch (err) {
-    return ResponseError(res, err.statusCode || 400, err.message);
+    return ResponseJson.responseError(
+      res,
+      err.statusCode || 400,
+      err.message,
+    );
   }
 }
 
@@ -69,15 +81,18 @@ async function deleteSubCategory(req, res) {
   try {
     const subCategory = await subCategoryService.destroy(req);
 
-    return res.status(200).json({
-      status: 'success',
-      message: 'success delete sub category',
-      data: {
-        subCategory,
-      },
-    });
+    return ResponseJson.responseSuccess(
+      res,
+      200,
+      'success delete sub category',
+      { subCategory },
+    );
   } catch (err) {
-    return ResponseError(res, err.statusCode || 400, err.message);
+    return ResponseJson.responseError(
+      res,
+      err.statusCode || 400,
+      err.message,
+    );
   }
 }
 
