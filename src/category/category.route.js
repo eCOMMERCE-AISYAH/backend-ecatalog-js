46e041c099b7ccfc008b19d63811888f73eec1dc
Ardhi Harry <ardhiharry@gmail.com>
2023-12-23 23:09:19 +0700

    merubah http method dari put ke patch

--- src/category/category.route.js
+++ src/category/category.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/api/category', categoryController.createCategory);
 router.get('/api/category', categoryController.getAllCategory);
 router.get('/api/category/:id', categoryController.getCategoryById);
-router.put('/api/category/:id', categoryController.updateCategory);
+router.patch('/api/category/:id', categoryController.updateCategory);
 router.delete('/api/category/:id', categoryController.deleteCategory);
 
 export default router;
--- src/subCategory/subCategory.route.js
+++ src/subCategory/subCategory.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/api/subCategory', subCategoryController.createSubCategory);
 router.get('/api/subCategory', subCategoryController.getAllSubCategory);
 router.get('/api/subCategory/:id', subCategoryController.getSubCategoryById);
-router.put('/api/subCategory/:id', subCategoryController.updateSubCategory);
+router.patch('/api/subCategory/:id', subCategoryController.updateSubCategory);
 router.delete('/api/subCategory/:id', subCategoryController.deleteSubCategory);
 
 export default router;
