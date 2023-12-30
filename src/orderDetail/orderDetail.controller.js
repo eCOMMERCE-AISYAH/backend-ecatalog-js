128fc982ad8fc0405e2f554a8d082eea3489fdae
bayeng <88835193+bayeng@users.noreply.github.com>
2023-12-30 14:53:15 +0700

    fix error notification domain

--- src/orderDetail/orderDetail.controller.js
+++
@@ -1,9 +0,0 @@
-import orderDetailService from './orderDetail.service.js';
-
-async function getOrderDetail(req, res) {
-  try {
-    const orderDetail = await orderDetailService.get(id);
-  } catch (e) {
-
-  }
-}
