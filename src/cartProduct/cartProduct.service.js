dd9be1f63ac4bc720a50edd1a4c3bededc91120d
bayeng <88835193+bayeng@users.noreply.github.com>
2023-12-30 12:02:28 +0700

    cartProduct domain

--- src/cartProduct/cartProduct.service.js
+++ src/cartProduct/cartProduct.service.js
@@ -1,30 +1,49 @@
 import prisma from '../../prisma/prismaClient.js';
 import ApiErrorHandling from '../../helper/apiErrorHandling.js';
 
-async function getAll(req) {
-  const { take, skip } = req.query;
+async function getAllByQuery(req) {
+  const { take, skip, cartid } = req.query;
 
-  let result;
-  if (take === undefined || skip === undefined) {
-    result = await prisma.cartProduct.findMany();
-  } else {
-    result = await prisma.cartProduct.findMany({
-      take,
-      skip,
-    });
+  const result = await prisma.cartProduct.findMany({
+    take: take !== undefined ? Number(take) : undefined,
+    skip: skip !== undefined ? Number(skip) : undefined,
+    where: {
+      cartId: cartid !== undefined ? cartid : undefined,
+    },
+    include: {
+      cart: true,
+      product: true,
+    },
+  });
+
+  return result;
+}
+
+async function get(id) {
+  const result = await prisma.cartProduct.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      cart: true,
+      product: true,
+    },
+  });
+
+  if (!result) {
+    throw new ApiErrorHandling(404, 'not found');
   }
 
   return result;
 }
 async function create(req) {
   const {
-    quantity, cartId, productId, notes, status,
+    quantity, cartId, productId, notes,
   } = req.body;
 
   const data = {
     quantity,
     notes,
-    status,
     cart: {
       connect: {
         id: cartId,
@@ -41,7 +60,12 @@ async function create(req) {
     data: {
       ...data,
     },
+    include: {
+      cart: true,
+      product: true,
+    },
   });
+
   if (!result) {
     throw new ApiErrorHandling(500, 'internal server error');
   }
@@ -49,7 +73,65 @@ async function create(req) {
   return result;
 }
 
+async function update(id, req) {
+  const {
+    quantity, cartId, productId, notes,
+  } = req.body;
+
+  const data = {
+    quantity,
+    notes,
+    cart: {
+      connect: {
+        id: cartId,
+      },
+    },
+    product: {
+      connect: {
+        id: productId,
+      },
+    },
+  };
+
+  const result = await prisma.cartProduct.update({
+    where: {
+      id,
+    },
+    data: {
+      ...data,
+    },
+    include: {
+      cart: true,
+      product: true,
+    },
+  });
+
+  if (!result) {
+    throw new ApiErrorHandling(500, 'internal server error');
+  }
+
+  return result;
+}
+
+async function destroy(id) {
+  const checkExisting = await get(id);
+  if (!checkExisting) {
+    throw new ApiErrorHandling(404, 'cart product not found');
+  }
+
+  const result = await prisma.cartProduct.delete({
+    where: {
+      id,
+    },
+  });
+
+  return result;
+}
+
 export default {
-  getAll,
+  getAllByQuery,
   create,
+  get,
+  destroy,
+  update,
 };
