cce83dbcffda9227ad3a77e09b45b55b08c13442
bayeng <88835193+bayeng@users.noreply.github.com>
2024-04-08 01:12:37 +0700

    update: orderHistory | update orderHistory create

--- src/orderHistory/orderHistory.route.js
+++ src/orderHistory/orderHistory.route.js
@@ -4,6 +4,5 @@ import orderHistoryController from './orderHistory.controller.js';
 const router = express.Router();
 
 router.get('/api/orderhistory', orderHistoryController.getOrderHistoryByQuery);
-router.post('/api/orderhistory', orderHistoryController.createOrderHistory);
 
 export default router;
