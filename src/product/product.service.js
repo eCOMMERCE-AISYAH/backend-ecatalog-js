4d9e7533855f49a949af546b6519cef8c55cbd6f
bayeng <88835193+bayeng@users.noreply.github.com>
2024-05-16 02:13:09 +0700

    fix: product and productImage | error handling

--- src/product/product.service.js
+++ src/product/product.service.js
@@ -44,7 +44,10 @@ async function create(req) {
   }
 
   // create product image
-  productImageService.create(req, result.id);
+  const createImage = await productImageService.create(req, result.id);
+  if (!createImage) {
+    throw new ApiErrorHandling(400, 'gambar tidak boleh kosong');
+  }
   return result;
 }
 
